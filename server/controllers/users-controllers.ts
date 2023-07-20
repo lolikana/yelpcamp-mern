96b9429cbd935bd21bd39150d54cfbabb11f1ae0
Lolikana <thomas.guiart@gmail.com>
2023-07-20 14:21:20 +0900

    delete get users route

--- server/controllers/users-controllers.ts
+++ server/controllers/users-controllers.ts
@@ -6,19 +6,6 @@ import { ExpressError } from './../utils';
 import { signupSchema } from './../utils/validations';
 
 export default {
-  getAll: (async (_req, res, next): Promise<void> => {
-    try {
-      const users = await User.find({});
-      res.json({ users: users.map(user => user.toObject({ getters: true })) });
-    } catch (err) {
-      const error = new ExpressError(
-        'Fetching users failed, please try again later.' || err,
-        500
-      );
-      next(error);
-    }
-  }) as RequestHandler,
-
   singup: (async (req, res, next) => {
     try {
       const { username, email } = req.body as ISignup;
--- server/routes/users-routes.ts
+++ server/routes/users-routes.ts
@@ -4,6 +4,4 @@ import users from './../controllers/users-controllers';
 
 export const router = Router();
 
-router.route('/').get(users.getAll);
-
 router.route('/signup').post(users.singup);
