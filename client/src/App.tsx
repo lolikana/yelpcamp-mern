ae3c59544211204a27ce687e841b939ba010100b
Lolikana <thomas.guiart@gmail.com>
2023-07-20 16:39:22 +0900

    create custom hook head title

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,15 @@
+import { useRef } from 'react';
+
 import styles from './App.module.scss';
+import useTitle from './hooks/use-title';
 
 function App() {
+  const headTitleRef = useRef<string>('HOMEPAGE');
+  useTitle(headTitleRef.current);
+
   return (
     <>
-      <h1 className={styles.title}>HOMEPAGE</h1>
+      <h1 className={styles.title}>{headTitleRef.current}</h1>
     </>
   );
 }
