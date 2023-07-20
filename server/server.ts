bf07ce34331a1549934b53bdeb0ba2e90b24bb80
Lolikana <thomas.guiart@gmail.com>
2023-07-20 09:26:08 +0900

    add utils express error and 404

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,15 @@
 import dotenv from 'dotenv';
 import express from 'express';
 
+import { ExpressError } from './utils';
+
 dotenv.config();
 
 const port = 3001;
 const app = express();
+
+app.all('*', (_req, _res, next) => {
+  next(new ExpressError('Page Not Found!!', 404));
+});
 
 app.listen(port, () => console.log(`Server started at port: ${port}`));
---
+++ server/utils/index.ts
@@ -0,0 +1,1 @@
+export { default as ExpressError } from './express-error';
