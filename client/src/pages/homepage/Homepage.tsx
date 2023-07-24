1d2d33eb4e15f1ca8988ef92354b5cbf53588b23
Lolikana <thomas.guiart@gmail.com>
2023-07-24 14:21:25 +0900

    add text homepage

--- client/src/pages/homepage/Homepage.tsx
+++ client/src/pages/homepage/Homepage.tsx
@@ -8,10 +8,14 @@ const Homepage = () => {
   useTitle(headTitleRef.current);
 
   return (
-    <section className={styles.homepage}>
-      <h1 data-testid="title" className={styles.title}>
-        {headTitleRef.current}
-      </h1>
+    <section className={`${styles.homepage} section-page`}>
+      <div className={styles.homepage__presentation}>
+        <h1 className={styles.homepage__presentation_title}>Welcome to Yelpcamp!</h1>
+        <p>
+          Jump right in and explore our many campgrounds. Feel free to share some of your
+          own and comment on others!
+        </p>
+      </div>
     </section>
   );
 };
