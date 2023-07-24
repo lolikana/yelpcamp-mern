d717af2a91246239a95cfdf5e91320229a07dfaa
Lolikana <thomas.guiart@gmail.com>
2023-07-24 14:39:18 +0900

    fix style for backdrop filter

--- client/src/components/layout/header/Header.tsx
+++ client/src/components/layout/header/Header.tsx
@@ -7,13 +7,15 @@ import styles from './Header.module.scss';
 const Header = () => {
   const navigate = useNavigate();
   return (
-    <header data-testid="header-component" className={styles.header}>
-      <div className={styles.logo} onClick={() => navigate('/')}>
-        <img src={CampLogo} alt="Logo website" />
-        <span>YourCampSite</span>
-      </div>
+    <>
+      <header data-testid="header-component" className={styles.header}>
+        <div className={styles.logo} onClick={() => navigate('/')}>
+          <img src={CampLogo} alt="Logo website" />
+          <span>YourCampSite</span>
+        </div>
+      </header>
       <Navbar />
-    </header>
+    </>
   );
 };
 
