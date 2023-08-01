55fbb7901a2250e1eb6e5595756fb84ee1887fdc
Lolikana <thomas.guiart@gmail.com>
2023-08-01 16:33:13 +0900

    auto-login

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,7 +10,7 @@ import Homepage from '@pages/homepage/Homepage';
 import Login from '@pages/login/Login';
 import RootLayout from '@pages/root-layout/RootLayout';
 import Signup from '@pages/signup/Signup';
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
 
 import { AuthContext } from './context/auth-context';
@@ -20,19 +20,56 @@ import {
 } from './pages/campgrounds/CampgroundDetail';
 import { loader as campgroundsLoader } from './pages/campgrounds/Campgrounds';
 
+let logoutTimer: Timeout;
 function App() {
   const [uid, setUid] = useState<string | null>(null);
   const [token, setToken] = useState<string | null>(null);
+  const [tokenExpirationDate, setTokenExpirationDate] = useState<string | null>(null);
 
-  const login = useCallback((uid: string, token: string) => {
+  const login = useCallback((uid: string, token: string, expirationDate?: string) => {
     setUid(uid);
     setToken(token);
+    const tokenExpirationDate =
+      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60).toISOString();
+    setTokenExpirationDate(tokenExpirationDate);
+    localStorage.setItem(
+      'userData',
+      JSON.stringify({
+        userId: uid,
+        token: token,
+        expiration: tokenExpirationDate
+      })
+    );
   }, []);
 
   const logout = useCallback(() => {
     setUid(null);
+    setTokenExpirationDate(null);
     setToken(null);
+    localStorage.removeItem('userData');
   }, []);
+
+  useEffect(() => {
+    if (token && tokenExpirationDate) {
+      const remainingTime =
+        new Date(tokenExpirationDate).getTime() - new Date().getTime();
+      console.log(remainingTime);
+      logoutTimer = setTimeout(logout, remainingTime);
+    } else {
+      clearTimeout(+logoutTimer);
+    }
+  }, [token, logout, tokenExpirationDate]);
+
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem('userData') as string) as {
+      userId: string;
+      token: string;
+      expiration: string;
+    };
+    if (storedData && storedData.token && new Date(storedData.expiration) > new Date()) {
+      login(storedData.userId, storedData.token, storedData.expiration);
+    }
+  }, [login]);
 
   let router;
   if (!token) {
