ae47c42e2cf9fbaeb5b613949c92055657a9acd4
Lolikana <thomas.guiart@gmail.com>
2023-08-31 13:39:24 +0900

    add options to cors middleware

--- server/server.ts
+++ server/server.ts
@@ -14,7 +14,9 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    optionsSuccessStatus: 200
   })
 );
 
