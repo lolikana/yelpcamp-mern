d0b068fd2ce424657756cc4a756aa3d37236d289
Lolikana <thomas.guiart@gmail.com>
2024-02-27 15:06:21 +0900

    fix mapbox-gl unclustered point properties

--- client/src/components/campgrounds/types.ts
+++ client/src/components/campgrounds/types.ts
@@ -1,10 +1,12 @@
+import { LngLatLike } from 'mapbox-gl';
+
 export type TCampground = {
   title: string;
   location: string;
   description: string;
   price: string;
   images?: FileList | never[];
-  geometry: { type: 'Point'; coordinates: [number, number] };
+  geometry: { type: 'Point'; coordinates: [number, number] | LngLatLike };
 };
 
 export type TResponseCampground = {
